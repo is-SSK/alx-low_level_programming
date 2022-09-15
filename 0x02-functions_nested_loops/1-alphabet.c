1a06e4f253d25a1dbe056b655f94ec867f6c4792
Sam Kimani <qollinssk@gmail.com>
2022-09-15 09:01:37 -0700

    2-One

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point 
+ * Description: A function that prints the alphabet in lowercase followed by a new line
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: Prints the absolute value of an interger
+ * @N: n is an interger
+ * Return: int
+ */
+
+int _abs(int n);
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
