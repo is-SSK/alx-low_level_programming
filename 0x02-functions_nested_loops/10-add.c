a6aa0ad6b852f461afc2f9a2432b7d7bcd81cb4f
Sam Kimani <qollinssk@gmail.com>
2022-09-15 18:03:07 -0700

    2-Three

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two intergers
+ * Description: add two intergers, returns sum
+ * @n1: interger 1
+ * @n2: interger 2
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
