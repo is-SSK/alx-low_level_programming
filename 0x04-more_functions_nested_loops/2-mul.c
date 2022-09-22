0704398b9b139fe962917ef651bcfa8d919204fb
Sam Kimani <qollinssk@gmail.com>
2022-09-22 07:07:52 -0700

    4-Two

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - function that multiplies two intergers.
+ * @a: interger 1
+ * @b: interger 2
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
