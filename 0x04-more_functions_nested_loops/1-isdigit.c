96ca97bb92472b47849b626f66fe50ccaf38eca6
Sam Kimani <qollinssk@gmail.com>
2022-09-22 05:27:01 -0700

    4-Zero

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit(0 through 9)
+ * @c: input value
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+
+#endif /* MAIN_H */
