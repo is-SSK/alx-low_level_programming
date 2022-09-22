bbdccba8c909946b8f22a1061617ff73507c1ba5
Sam Kimani <qollinssk@gmail.com>
2022-09-22 10:12:27 -0700

    4-Four

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: void
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (i < 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+	}
+	printf("Buzz\n");
+}
