d0afb399e6227fd206f863d45401ade2b4d90efd
Sam Kimani <qollinssk@gmail.com>
2022-10-16 19:11:39 +0300

    5-Four

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -20,10 +20,10 @@ max = i - 1;
 half = max / 2;
 while (half >= 0)
 {
-    first = s[max - half];
-    last = s[half];
-    s[half] = first;
-    s[max - half] = last;
-    half--;
+first = s[max - half];
+last = s[half];
+s[half] = first;
+s[max - half] = last;
+half--;
 }
 }
