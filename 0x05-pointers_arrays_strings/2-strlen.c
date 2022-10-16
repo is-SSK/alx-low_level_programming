f120247da70bc5012ca01cacb039dc441e398160
Sam Kimani <qollinssk@gmail.com>
2022-10-16 19:15:12 +0300

    5-Five

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,11 +8,11 @@
 
 int _strlen(char *s)
 {
-    int length = 0;
+int length = 0;
 
-    while (s[length])
-    {
-        length++;
-    }
-    return (length);
+while (s[length])
+{
+length++;
+}
+return (length);
 }
